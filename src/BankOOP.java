9ba625c7cc15238b5193b72bbb88537d002f8212
Ipelcaztreortega <118202882+Ipelcaztreortega@users.noreply.github.com>
2025-07-24 11:38:22 -0400

    First commit

---
+++ src/BankOOP.java
@@ -0,0 +1,21 @@
+public class BankOOP {
+    public static void main(String[] args) {
+
+        // This is creating an implicit default constructor since it is not declared, but since we have one, java uses it now
+//        BankAccount userAccount = new BankAccount(3213, 123.02, "John"); // With the constructor, now we can input the values here
+//        userAccount.setAccountBalance(123.02);
+//        userAccount.setAccountNumber(3213);
+//        userAccount.setCustomerName("John");
+
+
+        // This uses constructor chaining.
+        BankAccount userAccount = new BankAccount();
+        userAccount.withdrawBalance(64.02);
+        userAccount.depositFund(2314.2);
+        userAccount.describeAccount();
+
+        // Constructor overloading, calling the constructor with one parameter
+        BankAccount newAccount = new BankAccount("Joe");
+        System.out.println("Account Number:" + newAccount.getAccountNumber() + "; name " + newAccount.getCustomerName());
+    };
+};
---
+++ src/ForLoops.java
@@ -0,0 +1,38 @@
+public class ForLoops {
+    public static void main(String[] args) {
+        for (int counter = 1; counter <=5; counter++) {
+            System.out.println(counter);
+        };
+
+        // Using for loop without break but having a condition
+        int count = 0;
+        for (int i = 10; count < 3 && i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println("number " + i + " is a prime number");
+            };
+            count++;
+        }
+
+        for (double i = 7.5; i <= 10; i+= 0.25) {
+            double interestAmount = calculateInterest(100.00, i);
+            System.out.println("$100.00 at " + i + "% interest = $" + interestAmount);
+        }
+    };
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    };
+
+    public static boolean isPrime(int number) {
+        if (number <= 2) {
+            return (number == 2);
+        };
+
+        for (int divisor = 2; divisor <= number / 2; divisor++) {
+            if (number % divisor == 0) {
+                return false;
+            };
+        };
+        return true;
+    };
+}

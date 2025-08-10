c9bd0affc33599cda60580baaf1b1b3664f26741
Ipelcaztreortega <118202882+Ipelcaztreortega@users.noreply.github.com>
2025-08-09 23:45:17 -0400

    Packages, Arrays, ArrayList, NestedArrays

---
+++ src/Movie.java
@@ -0,0 +1,85 @@
+public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    };
+
+    public void watchMovie() {
+        // Calling built in java class, that returns class type info about the run time instance
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film");
+    };
+
+    // Anyone can call this method to get a movie instance  with the type and title
+
+    // This returns a new instance object is called a factory method, gives us a way to create an object without knowing the details of how to make one
+    public static Movie getMovie(String type, String title) {
+        return switch (type.toUpperCase().charAt(0)) { // Grabs the uppercase first letter
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
+};
+
+class Adventure extends Movie {
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie(); // This calls the base class watchMovie(), with the overriden extra
+        System.out.printf("..%s%n".repeat(3),
+                "Pleasant Scene",
+                "Scary Music",
+                "Something Bad Happens"
+                );
+    };
+
+    public void watchAdventure() {
+        System.out.println("Watching an adventure movie");
+    }
+}
+
+class Comedy extends Movie {
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie(); // This calls the base class watchMovie(), with the overriden extra
+        System.out.printf("..%s%n".repeat(3),
+                "Something funny happens",
+                "Something even funnier happens",
+                "Happy Ending"
+        );
+    };
+
+    public void watchComedy() {
+        System.out.println("Watching a comedy movie");
+    }
+};
+
+class ScienceFiction extends Movie {
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie(); // This calls the base class watchMovie(), with the overriden extra
+        System.out.printf("..%s%n".repeat(3),
+                "Bad Aliens",
+                "Space Guys",
+                "Blows up"
+        );
+    };
+
+    public void watchScienceFiction() {
+        System.out.println("Watching a science fiction");
+    }
+}
---
+++ src/dev/ipo/ArrayLists.java
@@ -0,0 +1,41 @@
+package dev.ipo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+}
+
+
+public class ArrayLists {
+    public static void main(String[] args) {
+//        Object[] groceryArray = new Object[3];
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apple", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+
+        // Because we have an array of objects, the compiler allows this
+        System.out.println(Arrays.toString(groceryArray));
+
+        // If you do not specify the type with ArrayList, it will specify type Object by default.
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        // Proper way to initialize, diamond operator, resizable so it's not a fixed size
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("Milk"));
+        groceryList.add(new GroceryItem("Oranges", "Produce", 5));
+
+        // We can add a new item using an index to see where we would like to have it
+        groceryList.add(0, new GroceryItem("Apple", "Produce", 6));
+
+        System.out.println(groceryList);
+
+    };
+}
